33956f0bbbba7ebfa137079fb6e6bc5cb14f3e19
Ильдар Уразбахтин <ildarurazbahtin@MacBook-Air-Ildar.local>
2025-03-25 23:20:01 +0300

    fixed error page 404

--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('',views.dashboard, name='dashboard'),
-    path('images/', views.images, name='images'),
-    path('people/', views.images, name='people'),
 ]
--- account/views.py
+++ account/views.py
@@ -26,13 +26,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def dashboard(request):
-    return render(request,
-            'account/dashboard.html',
-                  {'section': 'dashboard'}
-                 )
-
-def images(request):
-    return render(request, 'images.html')
-
-def people(request):
-    return render(request, 'people.html')
+    return render(request,'account/dashboard.html',{'section': 'dashboard'})
